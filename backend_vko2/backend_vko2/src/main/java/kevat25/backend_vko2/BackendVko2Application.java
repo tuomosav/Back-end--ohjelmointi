ce6717f185a08f42fc3abad8d507f27b41f99bcf
tuomo.savolainen@myy.haaga-helia.fi <tuomo.savolainen@myy.haaga-helia.fi>
2025-01-22 22:05:56 +0200

    Vko2 aloitettu

---
+++ backend_vko2/backend_vko2/src/main/java/kevat25/backend_vko2/BackendVko2Application.java
@@ -0,0 +1,13 @@
+package kevat25.backend_vko2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVko2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVko2Application.class, args);
+	}
+
+}

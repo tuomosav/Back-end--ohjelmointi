18efd049a7cc6ea41443e52c6aec846c428661b2
tuomo.savolainen@myy.haaga-helia.fi <tuomo.savolainen@myy.haaga-helia.fi>
2025-01-28 04:20:33 +0200

    1-4 Tehty

---
+++ backend_vko2/backend_vko2/src/main/java/kevat25/backend_vko2/web/HelloThymeleafController.java
@@ -0,0 +1,17 @@
+package kevat25.backend_vko2.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloThymeleafController {
+
+    @GetMapping("/hello")
+    public String nameAndAge(@RequestParam String name, int age, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "hello";
+    }
+}
